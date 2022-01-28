3b6ba41a8468d8c96f1cd82dee65915437c3c0e7
Espaker Kaminski <espaker101@gmail.com>
2022-01-28 15:19:25 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { MDBContainer } from "mdbreact";
+
+import Header from './components/header/header'
+
 import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'bootstrap-css-only/css/bootstrap.min.css'
+import 'mdbreact/dist/css/mdb.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MDBContainer className="app" fluid>
+      <Header />
+      
+    </MDBContainer>
   );
 }
 
---
+++ src/components/header/header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { MDBNavbar } from "mdbreact";
+
+function Header() {
+    return (
+        <MDBNavbar color="elegant-color" flex> 
+
+        </MDBNavbar>
+    )
+}
+
+
+export default Header;
